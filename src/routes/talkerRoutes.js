c264cb0de74dd0103562015d0a83685a903f3c98
David-Neto <davidrmachado@gmail.com>
2022-10-05 22:16:36 -0300

    Entrega do requisito 2

--- src/routes/talkerRoutes.js
+++ src/routes/talkerRoutes.js
@@ -1,16 +1,34 @@
 const fs = require('fs');
+
 const router = require('express').Router();
+
 const path = require('path');
+
+const talkerpath = path.resolve(__dirname, '../talker.json');
 
 router.get('/', async (_req, res) => {
     try {
-      const talkerpath = path.resolve(__dirname, '../talker.json');
       const data = await fs.readFileSync(talkerpath);
       res.status(200).json(JSON.parse(data));
       return data;
   } catch (error) {
     console.error(`Erro na leitura do arquivo: ${error}`);
   }
+});
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    try {
+      const data = fs.readFileSync(talkerpath);
+      const talkerData = JSON.parse(data).find((talker) => talker.id === Number(id));
+      if (talkerData) {
+        res.status(200).json(talkerData);
+      } else {
+        res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+      }
+    } catch (error) {
+      console.error(`Erro na leitura do arquivo: ${error}`);
+    }
 });
 
 module.exports = router;
